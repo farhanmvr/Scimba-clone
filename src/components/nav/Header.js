11f46abc084e8903b14c2316c4f3da17d7b32f23
Mohammed Farhan <farhanmvr@gmail.com>
2021-04-14 01:04:32 +0530

    add navbar

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <nav className="navbar navbar-expand fixed-top navbar-light mt-2">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          Scrimba
+        </Link>
+        <ul className="navbar-nav">
+          <li className="nav-item my-auto">
+            <Link
+              to="/"
+              className="nav-link px-3 py-1 btn-primary text-light"
+            >
+              Go Pro!
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-link link">
+              All Courses
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-link link">
+              Sign In
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-link link">
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const LandingPage = () => {
-  return <div>Landing Page</div>;
+  return <div className='mt-5'></div>;
 };
 
 export default LandingPage;
